ebfc574f267b51fe063f4420976084f5f3002edb
tom <thomasmaxwellsmith@gmail.com>
2020-02-29 21:52:02 +0000

    can convert to png

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,4 +1,6 @@
 import React, { useRef } from "react";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
 
 import { TrackType } from "types";
 import css from "./Tile.module.css";
@@ -23,19 +25,39 @@ const handleNewLines = (string: String) => {
 
 const Tile = (props: TileType) => {
   const ref = useRef(null);
-  console.log(ref.current);
+  console.log(ref);
   return (
-    <section ref={ref} className={css.container}>
-      {props.data.map((track, i) => (
-        <div key={i} className={css.track}>
-          <div className={css.main}>
-            <span className={css.artist}>{handleNewLines(track.Artist)} -</span>{" "}
-            <span className={css.song}>{handleNewLines(track.Song)}</span>
+    <>
+      <section ref={ref} className={css.container}>
+        {props.data.map((track, i) => (
+          <div key={i} className={css.track}>
+            <div className={css.main}>
+              <span className={css.artist}>
+                {handleNewLines(track.Artist)} -
+              </span>{" "}
+              <span className={css.song}>{handleNewLines(track.Song)}</span>
+            </div>
+            <div className={css.label}>({track.Label})</div>
           </div>
-          <div className={css.label}>({track.Label})</div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section>
+      <div>
+        <button
+          onClick={() => {
+            if (ref.current != null) {
+              const fragment = new DocumentFragment();
+              domtoimage
+                .toBlob(ref.current ?? fragment)
+                .then(function(blob: Blob) {
+                  saveAs(blob, "my-node.png");
+                });
+            }
+          }}
+        >
+          To PNG
+        </button>
+      </div>
+    </>
   );
 };
 
