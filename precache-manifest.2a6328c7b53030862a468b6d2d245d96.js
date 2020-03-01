937896f1c1d55040cef80b74ff88fb71e887d8e5
tom <thomasmaxwellsmith@gmail.com>
2020-03-01 17:14:50 +0000

    Updates

---
+++ precache-manifest.2a6328c7b53030862a468b6d2d245d96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd42591aea3e2f60c13b0695217fc56",
+    "url": "/amys/index.html"
+  },
+  {
+    "revision": "59acfb4df8947c582330",
+    "url": "/amys/static/css/main.001dc7a5.chunk.css"
+  },
+  {
+    "revision": "61c2476469986a84550e",
+    "url": "/amys/static/js/2.faecf54c.chunk.js"
+  },
+  {
+    "revision": "12c6de3bded80ca42de7069807ad84c6",
+    "url": "/amys/static/js/2.faecf54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59acfb4df8947c582330",
+    "url": "/amys/static/js/main.00fa4542.chunk.js"
+  },
+  {
+    "revision": "d276451d5650abeff921",
+    "url": "/amys/static/js/runtime-main.4a9ca935.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.adbb7907107ba556fee56ce578c10754.js"
+  "/amys/precache-manifest.2a6328c7b53030862a468b6d2d245d96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amys/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
