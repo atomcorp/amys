ac1d27b06b44872b7f3e91b1aa7b20ad5ca42650
atomcorp <thomasmaxwellsmith@gmail.com>
2020-06-03 17:00:30 +0100

    Updates

--- precache-manifest.d87d3e80fd6156643b85dbea72774b75.js
+++ precache-manifest.d87d3e80fd6156643b85dbea72774b75.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d45401ad39a6b1ad88c41881b603c6b0",
+    "revision": "0c294ca2b0622f56e36196c5f1fe2a43",
     "url": "/amys/index.html"
   },
   {
-    "revision": "e6689905111d6c6ce8e9",
-    "url": "/amys/static/css/main.1a903b07.chunk.css"
+    "revision": "ab2a528c1c69123e5fe0",
+    "url": "/amys/static/css/main.eb7bc51f.chunk.css"
   },
   {
     "revision": "61c2476469986a84550e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/amys/static/js/2.faecf54c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e6689905111d6c6ce8e9",
-    "url": "/amys/static/js/main.b3c2227d.chunk.js"
+    "revision": "ab2a528c1c69123e5fe0",
+    "url": "/amys/static/js/main.f3563220.chunk.js"
   },
   {
     "revision": "d276451d5650abeff921",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/amys/precache-manifest.f125ab4d6e0c519adb5f2e2ba49709a3.js"
+  "/amys/precache-manifest.d87d3e80fd6156643b85dbea72774b75.js"
 );
 
 self.addEventListener('message', (event) => {
