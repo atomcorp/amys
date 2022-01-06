005134d057485e6c0f9942e4d896cc17e324740c
atomcorp <thomasmaxwellsmith@gmail.com>
2022-01-06 11:19:18 +0000

    Updates

--- precache-manifest.64d21a79cc5317a1a377223a1d7bb918.js
+++ precache-manifest.64d21a79cc5317a1a377223a1d7bb918.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41fb29f94cb5f9b305a655dcaeee3154",
+    "revision": "4fcbab58e4dc926ec2799cc6ad9a4ee6",
     "url": "/amys/index.html"
   },
   {
-    "revision": "cfdd8052080412e4064d",
-    "url": "/amys/static/css/main.d1495ace.chunk.css"
+    "revision": "a2b56db7b33e9e761346",
+    "url": "/amys/static/css/main.473fd9eb.chunk.css"
   },
   {
     "revision": "61c2476469986a84550e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/amys/static/js/2.faecf54c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cfdd8052080412e4064d",
-    "url": "/amys/static/js/main.371641c5.chunk.js"
+    "revision": "a2b56db7b33e9e761346",
+    "url": "/amys/static/js/main.b7431359.chunk.js"
   },
   {
     "revision": "d276451d5650abeff921",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/amys/precache-manifest.b9909b256b24e5b62d405a38d3eea5fb.js"
+  "/amys/precache-manifest.64d21a79cc5317a1a377223a1d7bb918.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amys/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
